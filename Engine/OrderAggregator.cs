8151bce5414ed0483a6ab6e0680bd98706c79ef5
Nox-Lit <zacharie.constans@gmail.com>
2022-07-22 16:08:37 +0200

    Aggregators -- order, weighted sum, trival

---
+++ Engine/OrderAggregator.cs
@@ -0,0 +1,28 @@
+﻿namespace Engine;
+
+public class OrderAggregator<TValue> : Aggregator<TValue, List<TValue>>
+{
+    private Func<TValue, TValue, int> ComparatorFunction;
+
+    // private class Comparator : IComparer<TValue>
+    // {
+    //     private static Func<TValue, TValue, int> ComparatorFunc { get; set; } = null;
+    //     public int Compare(TValue left, TValue right)
+    //     {
+    //         ComparatorFunc = ComparatorFunction;
+    //         return ComparatorFunc(left, right);
+    //     }
+    // }
+    public OrderAggregator(Func<TValue, TValue, int> comparatorFunction)
+    {
+        this.ComparatorFunction = comparatorFunction;
+    }
+
+    public override List<TValue> Aggregate(List<TValue> items)
+    {
+        // return items.Sort((value1, value2) => ComparatorFunction(value1, value2));
+        List<TValue> result = new List<TValue>(items);
+        result.Sort((value1, value2) => ComparatorFunction(value1, value2));
+        return result;
+    }
+}
---
+++ Engine/WeightedAverage.cs
@@ -0,0 +1,26 @@
+﻿namespace Engine;
+
+public class WeightedAverage<TValue> : Aggregator<TValue, double>
+{
+    private Func<TValue, double> _value;
+    private Func<TValue, double> _weight;
+
+    public WeightedAverage(Func<TValue, double> valueFunc, Func<TValue, double> weightFunc)
+    {
+        _value = valueFunc;
+        _weight = weightFunc;
+    }
+
+    public override double Aggregate(List<TValue> items)
+    {
+        double sum = 0;
+        double totalWeight = 0;
+        foreach (var item in items)
+        {
+            sum += _value(item) * _weight(item);
+            totalWeight += _weight(item);
+        }
+
+        return totalWeight == 0 ? 0 : sum / totalWeight;
+    }
+}
