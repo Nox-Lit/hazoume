11f033bafb7066c2af0d9e25a246aeeb3122594d
Nox-Lit <zacharie.constans@gmail.com>
2022-07-22 16:05:41 +0200

    .NET project files

---
+++ Engine/FilterAggregator.cs
@@ -0,0 +1,22 @@
+﻿namespace Engine;
+
+public class FilterAggregator<TValue> : Aggregator<TValue, List<TValue>>
+{
+    private Func<TValue, bool> filter;
+
+    public FilterAggregator(Func<TValue, bool> filter)
+    {
+        this.filter = filter;
+    }
+
+    public override List<TValue> Aggregate(List<TValue> items)
+    {
+        List<TValue> filtered = new List<TValue>(items);
+        foreach (var item in items)
+        {
+            if (filter(item))
+                filtered.Remove(item);
+        }
+        return filtered;
+    }
+}
